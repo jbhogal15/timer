1c8bb4e2b22720b8ad690b71b6fe56cebe89d11b
Jaskarn <jaskarn15@gmail.com>
2022-04-12 23:11:58 +0000

    alarm function

---
+++ timer1.js
@@ -0,0 +1,23 @@
+let input = process.argv.slice(2);
+
+
+const alarm = function(input) {
+  let inputNum = input.map(element => Number(element));
+  let timeStamp = inputNum;
+  for (const t of timeStamp) {
+    if (t < 0) {
+      continue;
+    }
+    if (isNaN(t)) {
+      continue;
+    }
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, t * 1000);
+  }
+};
+
+console.log(alarm(input));
+
+
+
